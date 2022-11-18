993e042a3a1caa7d17090981bcffc1ddd63c882b
卫龙 <85797992+gwt805@users.noreply.github.com>
2022-11-19 01:55:58 +0800

    Add files via upload
    pro init

---
+++ notes-flask-app.py
@@ -0,0 +1,49 @@
+import json
+from flask import Flask, request, Response, render_template
+
+# 初始化web应用
+app = Flask(__name__, instance_relative_config=True)
+app.config['DEBUG'] = False
+
+
+def make_succ_empty_response():
+    data = json.dumps({'code': 0, 'data': {}})
+    return Response(data, mimetype='application/json')
+
+
+def make_succ_response(data):
+    data = json.dumps({'code': 0, 'data': data})
+    return Response(data, mimetype='application/json')
+
+
+def make_err_response(err_msg):
+    data = json.dumps({'code': -1, 'errorMsg': err_msg})
+    return Response(data, mimetype='application/json')
+
+
+@app.route('/', methods=['GET'])
+def login():
+    return render_template("login.html")
+
+
+@app.route('/', methods=['POST'])
+def logins():
+    params = request.get_json()
+    user = params['user']
+    pwd = params['pwd']
+    if user == pwd == "admin":
+        return make_succ_response("successful")
+    else:
+        return make_succ_response("error")
+
+
+@app.route('/notes')
+def notes():
+    try:
+        id = request.args["id"]
+        return render_template(f"notes/{id}-note.html")
+    except:
+        pass
+    return render_template("index.html")
+
+# app.run(host="0.0.0.0", port=80) # 本地调试打开
